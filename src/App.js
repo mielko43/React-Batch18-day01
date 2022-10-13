e39d7f4ee5d985e5379457ce6bc04ecb0b1c1755
Bagas <bagas.farid@gmail.com>
2022-10-13 22:47:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter/Counter';
+import CounterArrow from './Counter/CounterArrow';
+import CounterHook from './Counter/CounterHook';
+import Parent from './ParentChild/Parent';
+import EmployeeList from './List/EmployeeList';
+
+
+function App() {
+  return (
+    <div>
+      <EmployeeList/>
+    </div>
+  );
+}
+
+export default App;
